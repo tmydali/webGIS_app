85ade35c672ef68039fc82c20e7f16f26cd2cbb7
林彥呈 <f64066096@ncku.edu.tw>
2020-02-11 01:44:03 +0800

    able to upload geoJSON

--- js/basic_op.js
+++ js/basic_op.js
@@ -1,30 +1,76 @@
-export function clearLayer(layerGroup) {
+export default class Basics {
+	
+	static layerGroup;
+	
+	constructor(map, layerGroup) {
+		this.map = map;
+		Basics.layerGroup = layerGroup;
+	}
+	
+	setButtonCallback() {
+		// button callback
+		$("#clear_btn").click( () => clearLayer(Basics.layerGroup));
+		$("#buffer_btn").click( () => {
+			let buffered = buffer(Basics.layerGroup, 500);
+			L.geoJSON(buffered).addTo(Basics.layerGroup);
+		});
+		$('#upload_btn').click( () => {
+			$('#popup-content').html( 
+				'<h2>Choose your own GeoJSON file</h2>' +
+				'<input id="file" type="file" accept=".json, .geojson" />');
+			$('#popup-window').show();
+			$('#file').change(this.onFileLoad);
+			console.log($('#file'));
+		});
+		$('#close_btn').click( () => {
+			$('#popup-window').hide();
+		});
+	}
+
+	addPointListener() {
+		this.map.on('click', (e) => {
+			if(e.originalEvent.button == 0) {
+				//alert("You clicked the map at " + e.latlng);
+				L.marker(e.latlng).addTo(Basics.layerGroup);
+			}
+		});
+	}
+	
+	onFileLoad(event) {
+		var reader = new FileReader();
+		var onReaderLoad = (event) => {
+			try {
+				var obj = JSON.parse(event.target.result);
+				$('#popup-window').hide();
+				Basics.addToLayerGroup(obj)
+				console.log(obj);
+			}
+			catch(err) {
+				console.log(err);
+			}
+		}
+		reader.onload = onReaderLoad;
+		reader.readAsText(event.target.files[0]);
+	}
+	
+	static addToLayerGroup(obj) {
+		console.log(Basics.layerGroup);
+		L.geoJSON(obj).addTo(Basics.layerGroup);
+	}
+}
+
+function clearLayer(layerGroup) {
 	// layerGroup is an array
-	console.log(layerGroup);
+	console.log(layerGroup.getLayers());
 	layerGroup.clearLayers();
 	/*layerGroup.forEach( item => {
 		item.clearLayers();
 	});*/
 }
 
-export function buffer(srcLayer, distance) {
+function buffer(srcLayer, distance) {
 	var buffered = turf.buffer(srcLayer.toGeoJSON(), distance, {units: 'meters', steps: 64});
 	console.log(buffered);
 	return buffered;
 }
 
-function addPoint(e, layer) {
-	if(e.originalEvent.button == 0) {
-		//alert("You clicked the map at " + e.latlng);
-		L.marker(e.latlng).addTo(layer);
-	}
-}
-
-export function addPointListener(map, layer) {
-	map.on('click', function (e) {
-		if(e.originalEvent.button == 0) {
-			//alert("You clicked the map at " + e.latlng);
-			L.marker(e.latlng).addTo(layer);
-		}
-	});
-}
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-import * as Basics from './basic_op.js';
+import Basics from './basic_op.js';
 
-var mymap = L.map('mapid').setView([22.999, 120.220], 17);
+var map = L.map('mapid').setView([22.999, 120.220], 17);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 21,
@@ -8,24 +8,11 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   	zoomOffset: -1,
     id: 'mapbox/streets-v11',
     accessToken: 'pk.eyJ1IjoidG15ZGFsaSIsImEiOiJjazB3MzlrMXowdTUwM2lwZGlhMml5ZDduIn0.i56vbB-1XI6HPe_zEcDr1g'
-}).addTo(mymap);
+}).addTo(map);
 
-var layerGroup = L.layerGroup().addTo(mymap);
-Basics.addPointListener(mymap, layerGroup);
+var layerGroup = L.layerGroup().addTo(map);
+var Bass = new Basics(map, layerGroup);
+Bass.setButtonCallback();
+Bass.addPointListener();
 
-
-
-document.getElementById("clrMap").addEventListener("click", () =>
-	Basics.clearLayer(layerGroup));
-document.getElementById("bfr").addEventListener("click", buffer);
-
-
-function clearMap() {
-	Basics.clearLayer(layerGroup);
-}
-
-function buffer() {
-	let buffer = Basics.buffer(layerGroup, 500);
-	L.geoJSON(buffer).addTo(layerGroup);
-}
 
